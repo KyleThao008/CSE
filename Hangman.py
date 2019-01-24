b023bfc0a728d4a7b95f219980b080ef102cbde9
KyleThao008 <44372245+KyleThao008@users.noreply.github.com>
2019-01-24 08:40:06 -0800

    1/24

--- Hangman.py
+++ Hangman.py
@@ -3,14 +3,19 @@ import string
 words = ["mississippi", "california", "afghanistan", "japan", "hippopotamus", "germany", "panda", "fruit",
          "mongoose", "moose"]
 guesses = 8
-letter = string.ascii_lowercase
+letter = string.ascii_letters
 word = random.choice(words)
-guessed = []
 l_guessed = []
-string = word
+win = False
 
 while guesses > 0:
-    print(word)
+    output = []
+    for letter in word:
+        if letter in l_guessed:
+            output.append(letter)
+        else:
+            output.append("*")
+    print("".join(output))
     guessed = input("Letter. ")
     l_guessed.append(guessed)
     if guessed in word:
@@ -20,4 +25,9 @@ while guesses > 0:
         print("Wrong letter.")
         print("You have used", l_guessed, "letters already.""You now have", + guesses, "left.")
         guesses -= 1
-
+    if word is output:
+        win = True
+if win is False:
+    print("You have lost.")
+else:
+    print("You have won.")
