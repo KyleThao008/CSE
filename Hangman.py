1a95be5f2ea3e6a23043d8b53138ed2fa3a604ea
KyleThao008 <44372245+KyleThao008@users.noreply.github.com>
2019-01-25 08:43:17 -0800

    1/25

--- Hangman.py
+++ Hangman.py
@@ -9,13 +9,17 @@ l_guessed = []
 win = False
 
 while guesses > 0:
-    output = []
+    hidden_output = []
     for letter in word:
         if letter in l_guessed:
-            output.append(letter)
+            hidden_output.append(letter)
         else:
-            output.append("*")
-    print("".join(output))
+            hidden_output.append("*")
+    print("".join(hidden_output))
+    if "*" not in hidden_output:
+        print("You have won.")
+        guesses -= 100
+        continue
     guessed = input("Letter. ")
     l_guessed.append(guessed)
     if guessed in word:
@@ -25,9 +29,7 @@ while guesses > 0:
         print("Wrong letter.")
         print("You have used", l_guessed, "letters already.""You now have", + guesses, "left.")
         guesses -= 1
-    if word is output:
-        win = True
-if win is False:
-    print("You have lost.")
-else:
-    print("You have won.")
+
+    if guesses == 0:
+        print("You have lost.")
+
--- Notes S2.py
+++ Notes S2.py
@@ -1,24 +1,58 @@
-print("Hello World")
-# single line comment
-cars = 5
-driving = True
-# cars = 5 → "5"
+# print("Hello World")
+# # single line comment
+# cars = 5
+# driving = True
+# # cars = 5 → "5"
+#
+# print("I have %s cars." % cars)
+# """
+# age = input("How old are you?")
+# print("You're %s? That's cool I guess." % age)
+# """
+#
+# colors = ["blue", "yellow", "orange", "green",  "red"]
+# colors.append("cyan")
+#
+# print(colors)
+#
+# colors.pop(0)
+# print(len(colors))
+# print(colors)
+#
+# print(colors[2])
+#
+# for i in range(len(word)):
 
-print("I have %s cars." % cars)
-"""
-age = input("How old are you?")
-print("You're %s? That's cool I guess." % age)
-"""
+import random
+import string
+words = ["miss_issippi"]
+guesses = 8
+letter = string.ascii_letters
+word = random.choice(words)
+l_guessed = []
 
-colors = ["blue", "yellow", "orange", "green",  "red"]
-colors.append("cyan")
+while guesses > 0:
+    hidden_output = []
+    for letter in word:
+        if letter in l_guessed:
+            hidden_output.append(letter)
+        else:
+            hidden_output.append("*")
+    print("".join(hidden_output))
+    if "*" not in hidden_output:
+        print("You have won.")
+        guesses -= 100
+        continue
+    guessed = input("Letter. ")
+    l_guessed.append(guessed)
+    if guessed in word:
+        print("Letter found.")
+        print("You have used", l_guessed, "letters already.")
+    else:
+        print("Wrong letter.")
+        print("You have used", l_guessed, "letters already.""You now have", + guesses, "left.")
+        guesses -= 1
 
-print(colors)
+    if guesses == 0:
+        print("You have lost.")
 
-colors.pop(0)
-print(len(colors))
-print(colors)
-
-print(colors[2])
-
-for i in range(len(word)):
