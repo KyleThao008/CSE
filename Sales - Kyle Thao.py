4d7a326b10562e204c891517271f67d7e7685016
KyleThao008 <44372245+KyleThao008@users.noreply.github.com>
2019-05-10 09:46:22 -0700

    5/10

--- Sales - Kyle Thao.py
+++ Sales - Kyle Thao.py
@@ -5,21 +5,24 @@ with open("Sales Records.csv", 'r') as old_csv:
     reader = csv.reader(old_csv)
     print("Processing...")
     fruit_total = 0
+    clothes_total = 0
     for row in reader:
         if row[0] == "Region":
             continue
         item = row[2]
-        profit_number =float(row[13])
-        # cost_number = row[12]
-        # revenue_number = row[11]
+        profit_number = float(row[13])
+        cost_number = row[12]
+        revenue_number = row[11]
         # print("Total Profit: ", profit_number)
         # print("Total Cost: ", cost_number)
         # print("Total Revenue: ", revenue_number)
 
         if "Fruits" == item:
-            profit_number = float(row[13])
-            cost_number = row[12]
-            revenue_number = row[11]
-            print("Total Fruit Profit: ", profit_number)
-            # print("Total Fruit Cost: ", cost_number)
-            # print("Total Fruit Revenue: ", revenue_number)
+            fruit_total += profit_number
+    print("Total Fruit Profit", round(fruit_total, 2))
+
+    if "Clothes" == item:
+        clothes_total += profit_number
+print("Total Fruit Profit", round(clothes_total, 2))
+
+
